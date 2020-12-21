60b08c941178bac2f396355a78ab82ce31b9d36f
Matt Robinson <brimstone@the.narro.ws>
2020-12-21 15:41:25 -0500

    general: Disable SMB signing

--- static/scripts/general.ps1
+++ static/scripts/general.ps1
@@ -22,3 +22,6 @@ Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled False
 (New-Object System.Net.WebClient).DownloadFile('http://live.sysinternals.com/Procmon64.exe', 'c:\users\all users\desktop\ProcMon64.exe')
 (New-Object System.Net.WebClient).DownloadFile('http://live.sysinternals.com/procexp64.exe', 'c:\users\all users\desktop\procexp64.exe')
 (New-Object System.Net.WebClient).DownloadFile('http://live.sysinternals.com/Autoruns64.exe', 'c:\users\all users\desktop\Autoruns64.exe')
+
+Set-SmbServerConfiguration -RequireSecuritySignature $false -Force
+Set-SmbServerConfiguration -EnableSecuritySignature $false -Force
