ca76abeb2989e901b9c763fe5e4c374cc1757244
Matt Robinson <brimstone@the.narro.ws>
2021-04-25 21:15:19 -0400

    Publishing Sun 25 Apr 2021 09:15:19 PM EDT

--- en.search.min.44b04d4002b74603a7c94f3bcd274d1edbb41eb8bbe3b09dac8db406bfd6b9e2.js
+++ en.search.min.44b04d4002b74603a7c94f3bcd274d1edbb41eb8bbe3b09dac8db406bfd6b9e2.js
@@ -3,7 +3,7 @@ input.addEventListener('focus',init);input.addEventListener('keyup',search);docu
 const characterPressed=String.fromCharCode(event.charCode);if(!isHotkey(characterPressed)){return;}
 input.focus();event.preventDefault();}
 function isHotkey(character){const dataHotkeys=input.getAttribute('data-hotkeys')||'';return dataHotkeys.indexOf(character)>=0;}
-function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/flexsearch.min.js');loadScript('/en.search-data.min.abd2beca4f2b73202409de3ef2bea690798f9ab4c37d2564c5492aa2a87f83cb.js',function(){input.required=false;search();});}
+function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/learn-hacking/flexsearch.min.js');loadScript('/learn-hacking/en.search-data.min.63ef31a21d8adebf2bf76c826722616f3660373d387f1a0c17724cad6e3d5dc5.js',function(){input.required=false;search();});}
 function search(){while(results.firstChild){results.removeChild(results.firstChild);}
 if(!input.value){return;}
 const searchHits=window.bookSearchIndex.search(input.value,10);searchHits.forEach(function(page){const li=element('<li><a href></a><small></small></li>');const a=li.querySelector('a'),small=li.querySelector('small');a.href=page.href;a.textContent=page.title;small.textContent=page.section;results.appendChild(li);});}
---
+++ sw.min.2403c3e27886e0bfb1c430c8cbbdfbb1cca624680d3c5aa34e177f9a13dd7b68.js
@@ -0,0 +1,1 @@
+if(navigator.serviceWorker){navigator.serviceWorker.register("/learn-hacking/sw.js",{scope:"/learn-hacking/"});}
